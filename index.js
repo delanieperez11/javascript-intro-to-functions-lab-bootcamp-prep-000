bc0a806f3d8a47df77b0fa04e8945e180b3e859c
Delanie Perez <delanieperez11@gmail.com>
2018-04-27 22:08:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ function logShout (string) {
 function logWhisper (string) {
   console.log(string.toLowerCase( ));
 }
-function sayHiToGrandma(string)
+function sayHiToGrandma(string) {
+  if string.toLowerCase === lowercase
+  return 
+}
