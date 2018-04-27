fe9cd3db79e0dcf6831e7a5073123b0d6d165f6d
Delanie Perez <delanieperez11@gmail.com>
2018-04-27 22:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper (string) {
   console.log(string.toLowerCase( ));
 }
 function sayHiToGrandma(string) {
-  if (string).toLowerCase === lowercase
+  if (string === lowercase)
   return "I can't hear you"
 }
