e68016f2528d5211b5b168ba9f21f0b770b94422
Delanie Perez <delanieperez11@gmail.com>
2018-04-27 22:16:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function logShout (string) {
 function logWhisper (string) {
   console.log(string.toLowerCase( ));
 }
+function sayHiToGrandma (string) {
+  var lowercase = "I can't hear you!";
+  var uppercase = "YES INDEED!";
+  var mixedCase = "I love you, too.";
+  if (string.toLowerCase(aWord) === aWord) {
+    return cantUnswer;
+  }
+  else if (aWord.toUpperCase(aWord) === aWord) {
+    return yesUnswer;
+  }
+  else if ("I love you, Grandma." === aWord) {
+    return lovUnswer
+  }
